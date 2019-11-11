c40562ec1dd86ed9bb845ded64c9c3c650f0c327
Ana Pereyra <pereyra.ana12@gmail.com>
2019-11-11 20:37:36 -0300

    changing cron config

--- registro-comidas-service/cronJobService.js
+++ registro-comidas-service/cronJobService.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 var emailService = require('./emailService');
 
-var task = cron.schedule('59 23 * * 0', function () {
+var task = cron.schedule('59 2 * * 1', function () {
     console.log('Running Cron Job');
     emailService.sendEmail();
 });
